ae7c73723fbea92d8df07a63f95f5c8855d34918
zoharod <47798450+zoharod@users.noreply.github.com>
2019-05-23 19:46:24 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,30 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
